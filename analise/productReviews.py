33a76158729579208c021b4d0d4fbfc953daa6b7
Vitor <vitorhug97@gmail.com>
2018-02-17 00:51:29 -0200

    Base de dados amazon

--- analise/productReviews.py
+++ analise/productReviews.py
@@ -1,8 +1,18 @@
-import nltk
-from nltk.corpus import product_reviews_1
+from nltk.corpus import PlaintextCorpusReader
+import json
+from nltk.tokenize import sent_tokenize, word_tokenize
 
-nokia_reviews = product_reviews_1.reviews('Nokia_6610.txt')
+data = json.load(open(r'C:\Users\Vitor\Documents\TCC\Base de dados\TVs\B00B93KG1A.json'))
 
-print(len(nokia_reviews))
+print(str(data["Reviews"][0]["Content"]))
+review = str(data["Reviews"][0]["Content"])
 
-print(nokia_reviews[0])
+print(word_tokenize(review))
+
+categoria = 'neg'
+if float(data["Reviews"][0]["Overall"]) >= 3.0:
+    categoria = 'pos'
+
+print(categoria)
+
+print(len(data["Reviews"]))
