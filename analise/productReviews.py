e3773589a40034fc517efd85299fb4a0d402e2c6
Vitor <vitorhug97@gmail.com>
2018-02-19 23:30:19 -0300

    Base de revisoes Trip Advisor balanceada

--- analise/productReviews.py
+++ analise/productReviews.py
@@ -2,9 +2,7 @@ from nltk.corpus import PlaintextCorpusReader
 import json
 from nltk.tokenize import sent_tokenize, word_tokenize
 
-data = json.load(open(r'C:\Users\Vitor\Documents\TCC\Base de dados\TripAdvisorJson\json\91703.json'))
 
-#C:\Users\Vitor\Documents\TCC\Base de dados\TVs\B00B93KG1A.json 0 negativas 2623 positivas
 #data = json.load(open(r'C:\Users\Vitor\Documents\TCC\Base de dados\TripAdvisorJson\json\260444.json')) 469 negativas Positivas = 2052
 
 #data = json.load(open(r'C:\Users\Vitor\Documents\TCC\Base de dados\TripAdvisorJson\json\774804.json')) Negativas = 118 Positivas = 2097
@@ -12,19 +10,65 @@ data = json.load(open(r'C:\Users\Vitor\Documents\TCC\Base de dados\TripAdvisorJs
 #data = json.load(open(r'C:\Users\Vitor\Documents\TCC\Base de dados\TripAdvisorJson\json\2515499.json')) Negativas = 472 Positivas = 3930
 
 #data = json.load(open(r'C:\Users\Vitor\Documents\TCC\Base de dados\TripAdvisorJson\json\91703.json'))Negativas = 461 Positivas = 3802
+
+data1 = json.load(open(r'C:\Users\Vitor\Documents\TCC\Base de dados\TripAdvisorJson\json\260444.json'))
+data2 = json.load(open(r'C:\Users\Vitor\Documents\TCC\Base de dados\TripAdvisorJson\json\774804.json'))
+data3 = json.load(open(r'C:\Users\Vitor\Documents\TCC\Base de dados\TripAdvisorJson\json\2515499.json'))
+
 neg = 0
 pos = 0
-for review in data["Reviews"]:
+
+reviews_pos = []
+reviews_neg = []
+
+for review in data1["Reviews"]:
 
     if float((review["Ratings"]["Overall"])) <=2:
+        objeto = word_tokenize(str(review["Content"])), 'neg'
+        reviews_neg.append(objeto)
         neg +=1
+
     if float((review["Ratings"]["Overall"])) >=4:
+        objeto = word_tokenize(str(review["Content"])), 'pos'
+        reviews_pos.append(objeto)
         pos +=1
 
-print(data["Reviews"][0])
-print(data["Reviews"][0]["Ratings"])
-print(float(data["Reviews"][0]["Ratings"]["Overall"]))
+for review in data2["Reviews"]:
+
+    if float((review["Ratings"]["Overall"])) <=2:
+        objeto = word_tokenize(str(review["Content"])), 'neg'
+        reviews_neg.append(objeto)
+        neg +=1
+    if float((review["Ratings"]["Overall"])) >=4:
+        objeto = word_tokenize(str(review["Content"])), 'pos'
+        reviews_pos.append(objeto)
+        pos +=1
+
+for review in data3["Reviews"]:
+
+    if float((review["Ratings"]["Overall"])) <=2:
+        objeto = word_tokenize(str(review["Content"])), 'neg'
+        reviews_neg.append(objeto)
+        neg +=1
+    if float((review["Ratings"]["Overall"])) >=4:
+        objeto = word_tokenize(str(review["Content"])), 'pos'
+        reviews_pos.append(objeto)
+        pos +=1
+
+
+
+
+
 print("Negativas =", neg)
 print("Positivas =", pos)
+base_balanceada = []
 
+for i in range(0, 1000):
+    base_balanceada.append(reviews_pos[i])
+    base_balanceada.append(reviews_neg[i])
+
+    
+print(len(reviews_neg))
+print(len(reviews_pos))
+print(len(base_balanceada))
 
